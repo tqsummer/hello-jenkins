53f4877eb51cb692eb9362d99705ffcb33f7bbf4
fangxiangqian <tqsummer@gmail.com>
2023-11-27 15:31:08 +0800

    add project

---
+++ jenkins-app01/src/main/java/com/study/hello/jenkins/app01/JenkinsApp01Application.java
@@ -0,0 +1,16 @@
+package com.study.hello.jenkins.app01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : fangxiangqian
+ * @created : 11/27/2023
+ **/
+@SpringBootApplication
+public class JenkinsApp01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApp01Application.class, args);
+    }
+}
---
+++ jenkins-app01/src/main/java/com/study/hello/jenkins/app01/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.study.hello.jenkins.app01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author : fangxiangqian
+ * @created : 11/27/2023
+ **/
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
