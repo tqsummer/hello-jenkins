17150f82f644a4082383089be6ab543ca307bf96
fangxiangqian <tqsummer@gmail.com>
2024-04-15 20:43:14 +0800

    add helloEnv

--- jenkins-app01/src/main/java/com/study/hello/jenkins/app01/controller/TestController.java
+++ jenkins-app01/src/main/java/com/study/hello/jenkins/app01/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.study.hello.jenkins.app01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author : fangxiangqian
@@ -16,5 +20,20 @@ public class TestController {
     @ResponseBody
     public String hello() {
         return "hello v0.0.17 canary\n<br/>";
+    }
+
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/helloEnv")
+    public String helloEnv(HttpServletRequest request) {
+        // 获取环境变量参数并打印
+        String paramValue = env.getProperty(request.getParameter("PARAM_NAME"));
+
+        // 打印环境变量参数
+        String message = "PARAM_NAME: " + request.getParameter("PARAM_NAME") + ", PARAM_VALUE:" + paramValue + "\n";
+
+        return message;
     }
 }
